8e50ff99a5da53c8835c5eb115969f903dab4b27
Isaac Abraham <isaacabraham@Isaacs-MacBook-Air.local>
2026-05-01 20:54:16 -0600

    fix: production stability and node version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+  return children;
 }
