69361f7492fcbc6c0737df3de830b33b4d35aa36
Isaac Abraham <isaacabraham@Isaacs-MacBook-Air.local>
2026-05-01 21:46:52 -0600

    fix: sync localePrefix with middleware to resolve redirect loops

--- navigation.ts
+++ navigation.ts
@@ -1,7 +1,7 @@
 import {createNavigation} from 'next-intl/navigation';
  
 export const locales = ['en', 'am', 'ti', 'ar'] as const;
-export const localePrefix = 'always'; // Default
+export const localePrefix = 'as-needed';
  
 export const {Link, redirect, usePathname, useRouter} =
   createNavigation({locales, localePrefix});
